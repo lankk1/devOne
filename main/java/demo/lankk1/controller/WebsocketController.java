423306b5a16c4042f44d2c26c6328dca88f68072
lankk1 <835264673@qq.com>
2018-07-25 13:28:11 +0800

    测试用

---
+++ main/java/demo/lankk1/controller/WebsocketController.java
@@ -0,0 +1,7 @@
+package demo.lankk1.controller;
+
+/**
+ * Created by dan3 on 2018/7/16.
+ */
+public class WebsocketController {
+}
---
+++ main/java/demo/lankk1/service/impl/WebsocketServiceImpl.java
@@ -0,0 +1,9 @@
+package demo.lankk1.service.impl;
+
+import demo.lankk1.service.WebsocketService;
+
+/**
+ * Created by dan3 on 2018/7/16.
+ */
+public class WebsocketServiceImpl implements WebsocketService {
+}
